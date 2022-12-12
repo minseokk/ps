fb516f05773f9f50a67c8dacbf5df15c4a8a5f2e
minseokk <minseokk@postech.ac.kr>
2022-12-12 16:56:19 +0900

    bj 11725 solved

--- baekjoon/11725.cpp
+++ baekjoon/11725.cpp
@@ -1,57 +1,46 @@
 #include <iostream>
-#include <list> 
+#include <vector> 
 #include <stack>
 
 using namespace std;
 
-class Pair {
-    public:
-    int a, b;
-
-    Pair(int _a, int _b) {
-        a = _a;
-        b = _b;
-    }
-};
-
 int main () {
     int n;
     int res[100000];
-    list<Pair> input_list;
-    list<Pair>::iterator iter; 
+    bool visited[100000] = {false};
     stack<int> stack;
     int tmp_a, tmp_b;
+    int child;
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n;
+    vector<int> graph[n+1];
 
     for (int i = 0; i < n-1; i++) {
         cin >> tmp_a;
         cin >> tmp_b;
-        input_list.push_back(Pair(tmp_a, tmp_b));
+        graph[tmp_a].push_back(tmp_b);
+        graph[tmp_b].push_back(tmp_a);
     }
 
     stack.push(1);
+    visited[1] = true;
 
     while (!stack.empty()) {
-        for (iter = input_list.begin(); iter != input_list.end();) {
-            if (iter->a == stack.top()) {
-                res[iter->b] = stack.top();
-                stack.push(iter->b);
-                input_list.erase(iter);
-                iter = input_list.begin();
-                continue;
-            } else if (iter->b == stack.top()) {
-                res[iter->a] = stack.top();
-                stack.push(iter->a);
-                input_list.erase(iter);
-                iter = input_list.begin();
+        for (int i = 0; i < graph[stack.top()].size();) {
+            child = graph[stack.top()][i];
+            if (!visited[child]) {
+                visited[child] = true;
+                res[child] = stack.top();
+                stack.push(child);
+                i = 0;
                 continue;
             }
-            iter++;
+            i++;
         }
+        tmp_a = stack.top();
         stack.pop();
     }
 
